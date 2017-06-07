9be15920078668bf63850e10840881f7b432b4e1
Lebowski <seregbi4@gmail.com>
2017-06-07 21:24:01 +0300

    First java homework, exercises 6

---
+++ Java1_HW1_6.java
@@ -0,0 +1,13 @@
+class Zadanie7
+{
+	public static void main (String[] args)
+	{
+		String name = "Сергей";
+		printName(name);
+	}
+	public static void printName (String name)
+	{
+		
+		System.out.println("Привет, " + name);
+	}
+}
