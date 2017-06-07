3832a6e29d8a55b0fcbab2ef7db5a42d054d9d2c
Lebowski <seregbi4@gmail.com>
2017-06-07 21:22:30 +0300

    My first Java Homework

---
+++ Java1_HW1_1.java
@@ -0,0 +1,23 @@
+class Zadanie2
+{
+	public static void main (String[] args)
+	{
+		char bukva = 'М';
+		String stroka = "Второе задание";
+		int number = 365;
+		short a = 16;
+		long b = 64;
+		double g = 1;
+		float temp = 36.6f;
+		boolean result = true;
+		System.out.println("Буква " + bukva);
+		System.out.println("Название " + stroka);
+		System.out.println("Количество дней " + number);
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("g = " + g);
+		System.out.println("Температура " + temp);
+		System.out.println("Ответ " + result);
+	}
+
+}
